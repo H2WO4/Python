bd831c682e77ae5e8c5fa1fe766d0ac14aa0090b
H2WO4 <arthurlamidel@gmail.com>
2021-04-15 21:01:37 +0200

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -1,5 +1,4 @@
-from typing import Any, Callable, Dict, Tuple
-from inspect import getsource
+from typing import Any, Callable, Dict, List, Tuple, Union
 
 class Card:
 
@@ -22,7 +21,7 @@ class Card:
 
 
 class Modifier:
-    def __init__(self, name: str, priority: Tuple[int, int, int], varsModif: Dict[str, Tuple[str, int]], cost: int = 0, use: str = "", can_use: str = "") -> None:
+    def __init__(self, name: str, priority: Tuple[int, int, int], varsModif: Dict[str, Tuple[str, int]], cost: int = 0, use: Union[str, List[str]] = "", can_use: Union[str, List[str]] = "") -> None:
         self.name = name
         self.priority = priority
         self.cost = cost
@@ -40,7 +39,7 @@ class Modifier:
 
 
 def create_card(name: str, *modifiers: Modifier) -> Card:
-    vars = {"atk": 0, "draw": 0}
+    vars = {"atk": 0, "def": 0, "draw": 0}
     cost = 0
     use = "def card_use_{}(card, source, target):\n".format(Card.total) + "\tprint(\"Used {} energy\".format(card.cost))\n"
     can_use = "def card_can_use_{}(card, source, target):\n\tresult = True\n".format(Card.total)
@@ -56,11 +55,20 @@ def create_card(name: str, *modifiers: Modifier) -> Card:
     
     for i in sorted(modifiers, key = lambda x: x.priority[1]):
         if i.use != "":
-            use += "\t" + i.use + "\n"
+            if isinstance(i.use, str):
+                use += "\t" + i.use + "\n"
+            else:
+                for j in i.use:
+                    use += "\t" + j + "\n"
+
     
     for i in sorted(modifiers, key = lambda x: x.priority[2]):
         if i.can_use != "":
-            can_use += "\t" + i.can_use + "\n"
+            if isinstance(i.can_use, str):
+                can_use += "\t" + i.can_use + "\n"
+            else:
+                for j in i.can_use:
+                    can_use += "\t" + j + "\n"
     
     can_use += "\treturn result\n"
     for i in vars:
@@ -77,16 +85,21 @@ def create_card(name: str, *modifiers: Modifier) -> Card:
     return Card(name, "", cost, vars, eval("card_use_{}".format(Card.total)), eval("card_can_use_{}".format(Card.total)))
 
 
-basicDamage = Modifier("Basic Damage", (0, 50, 100), {"atk": ("plus", 9)}, 1, "print(\"Deal {} damage\".format(card.vars['atk']))", "")
+basicDamage = Modifier("Basic Damage", (0, 50, 100), {"atk": ("plus", 9)}, 1, "print(\"Deal {} damage\".format(card.vars['atk']))")
+doubleDamage = Modifier("Double Damage", (0, 50, 100), {"atk": ("plus", 4)}, 1, ["print(\"Deal {} damage\".format(card.vars['atk']))", "print(\"Deal {} damage\".format(card.vars['atk']))"])
 damageMultiplexer = Modifier("Damage Muliplexer", (5, 100, 100), {"atk": ("times", 2)}, 1)
 costDown = Modifier("Cost Down", (51, 100, 100), {"atk": ("times", 0.7)}, -1)
-drawOne = Modifier("Draw One", (50, 51, 100), {"atk": ("plus", -2), "draw": ("plus", 1)})
-drawTwo = Modifier("Draw Two", (50, 51, 100), {"atk": ("plus", -5), "draw": ("plus", 2)})
+drawOne = Modifier("Draw One", (50, 51, 100), {"atk": ("times", 0.85), "draw": ("plus", 1)})
+drawOneE = Modifier("Draw One", (50, 51, 100), {"draw": ("plus", 1)}, 1)
+drawTwo = Modifier("Draw Two", (50, 51, 100), {"atk": ("times", 0.75), "draw": ("plus", 2)})
+
 
 A = create_card("Test", basicDamage, damageMultiplexer, drawOne, costDown)
-A.use(0, 0)
+if A.can_use(0, 0):
+    A.use(0, 0)
 
 print()
 
-B = create_card("Test", basicDamage, damageMultiplexer, drawTwo, costDown)
-B.use(0, 0)
+B = create_card("Test", doubleDamage, damageMultiplexer, drawOneE)
+if B.can_use(0, 0):
+    B.use(0, 0)
